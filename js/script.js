1141af50561b4a8c78c9ec8cd8a2ce4ddb128fb8
tauseefsh <31163529+tauseefsh@users.noreply.github.com>
2019-01-07 10:55:54 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,160 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+    scrollBar: true,
+    afterRender: function(index, nextIndex, direction){
+      var el = $('.intro');
+      el.paroller({
+        factor: 0.5,
+        type: 'background',
+        direction: 'vertical'
+      });
+    },
+    /* We need reinitialize paroller on window resize event */
+    afterResize: function() {
+      var el = $('.intro');
+      el.paroller({
+        factor: 0.5,
+        type: 'background',
+        direction: 'vertical'
+      });
+    }
+  });
+});
+
+/* This is for Site NAvigation */
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
+
+
+/* This is for Slider  */
+
+/**
+ * @Titulo: Slider Responsivo
+ * @Autor: Creaticode
+ * @URL: http://creaticode.com 
+ */
+$(function() {
+	/** -----------------------------------------
+	 * Modulo del Slider 
+	 -------------------------------------------*/
+	 var SliderModule = (function() {
+	 	var pb = {};
+	 	pb.el = $('#slider');
+	 	pb.items = {
+	 		panels: pb.el.find('.slider-wrapper > li'),
+	 	}
+
+	 	// Interval del Slider
+	 	var SliderInterval,
+	 		currentSlider = 0,
+	 		nextSlider = 1,
+	 		lengthSlider = pb.items.panels.length;
+
+	 	// Constructor del Slider
+	 	pb.init = function(settings) {
+	 		this.settings = settings || {duration: 8000};
+	 		var items = this.items,
+	 			lengthPanels = items.panels.length,
+	 			output = '';
+
+	 		// Insertamos nuestros botones
+	 		for(var i = 0; i < lengthPanels; i++) {
+	 			if(i == 0) {
+	 				output += '<li class="active"></li>';
+	 			} else {
+	 				output += '<li></li>';
+	 			}
+	 		}
+
+	 		$('#control-buttons').html(output);
+
+	 		// Activamos nuestro Slider
+	 		activateSlider();
+	 		// Eventos para los controles
+	 		$('#control-buttons').on('click', 'li', function(e) {
+	 			var $this = $(this);
+	 			if(!(currentSlider === $this.index())) {
+	 				changePanel($this.index());
+	 			}
+	 		});
+
+	 	}
+
+	 	// Funcion para activar el Slider
+	 	var activateSlider = function() {
+	 		SliderInterval = setInterval(pb.startSlider, pb.settings.duration);
+	 	}
+
+	 	// Funcion para la Animacion
+	 	pb.startSlider = function() {
+	 		var items = pb.items,
+	 			controls = $('#control-buttons li');
+	 		// Comprobamos si es el ultimo panel para reiniciar el conteo
+	 		if(nextSlider >= lengthSlider) {
+	 			nextSlider = 0;
+	 			currentSlider = lengthSlider-1;
+	 		}
+
+	 		controls.removeClass('active').eq(nextSlider).addClass('active');
+	 		items.panels.eq(currentSlider).fadeOut('slow');
+	 		items.panels.eq(nextSlider).fadeIn('slow');
+
+	 		// Actualizamos los datos del slider
+	 		currentSlider = nextSlider;
+	 		nextSlider += 1;
+	 	}
+
+	 	// Funcion para Cambiar de Panel con Los Controles
+	 	var changePanel = function(id) {
+	 		clearInterval(SliderInterval);
+	 		var items = pb.items,
+	 			controls = $('#control-buttons li');
+	 		// Comprobamos si el ID esta disponible entre los paneles
+	 		if(id >= lengthSlider) {
+	 			id = 0;
+	 		} else if(id < 0) {
+	 			id = lengthSlider-1;
+	 		}
+
+	 		controls.removeClass('active').eq(id).addClass('active');
+	 		items.panels.eq(currentSlider).fadeOut('slow');
+	 		items.panels.eq(id).fadeIn('slow');
+
+	 		// Volvemos a actualizar los datos del slider
+	 		currentSlider = id;
+	 		nextSlider = id+1;
+	 		// Reactivamos nuestro slider
+	 		activateSlider();
+	 	}
+
+		return pb;
+	 }());
+
+	 SliderModule.init({duration: 4000});
+
+});
